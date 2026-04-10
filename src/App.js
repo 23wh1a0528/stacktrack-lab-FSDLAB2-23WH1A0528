1437a4958532ebe1fbad424bc8d3b6ac1e5ae825
23wh1a0528 <23wh1a0528@bvrithyderabad.edu.in>
2026-04-10 21:06:51 +0530

    [update] updated App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div>
-      <Home />
-
-      {/* TODO: Replace this placeholder with Routes */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+    </Router>
   );
 }
 
