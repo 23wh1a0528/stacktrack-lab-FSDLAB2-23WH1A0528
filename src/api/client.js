86ae4d33b8f365e8ef3a890cd941cc71864ab06e
23wh1a0528 <23wh1a0528@bvrithyderabad.edu.in>
2026-04-10 21:12:19 +0530

    [update] updated client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,2 +1,16 @@
 // TODO: Set the base URL for your API
 
+const BASE_URL = "http://bvrithcloud.com";
+
+export const get = async (endpoint) => {
+    const response = await fetch(`${BASE_URL}${endpoint}`,{
+        method: "GET",
+        header: {
+            "Content-Type":"application/json",
+            "x-student-id":"23WH1A0528" 
+        }
+    });
+
+    const data = await response.json();
+    return data;
+}
